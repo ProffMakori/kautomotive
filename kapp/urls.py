189af86baca7eb676bb3d4894857373e3cb58308
ProffMakori <jamesmn2004@gmail.com>
2024-03-06 13:10:05 +0300

    j

---
+++ kapp/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path
+from kapp import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('inner/', views.inner, name='inner'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('registration/', views.register, name='registration'),
+    path('login/', views.login, name='login'),
+    path('home/', views.home, name='login'),
+]
